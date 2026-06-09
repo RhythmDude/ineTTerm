42273647b60cb4f3f05445f344f6191e73270c73
RhythmDude <reason052@gmail.com>
2026-06-08 22:01:46 -0400

    Update boot.js

--- sys/apps/sysApps/boot.js
+++ sys/apps/sysApps/boot.js
@@ -1,6 +1,8 @@
 getEnv("version").then((version) => {
   print("IneTTerm web terminal environment " + version);
-  runApp("byName", "Test");
+
 }).catch((err) => {
   print("Failed to load environment: " + err);
 });
+
+runApp("byName", "Test");
